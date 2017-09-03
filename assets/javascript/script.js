12f5b4517d15fae7ca93c2e997e5b931aed75ff0
billscarp <billscarp@gmail.com>
2017-09-03 12:04:31 -0400

    hooking up to giphy

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -35,3 +35,35 @@ $(document).ready(function () {
     renderButtons();
 
 });
+
+
+    // Here I am going to add the API and ajax the way we did in our homework to bring in the gifs
+    $("button").on("click", function() {
+        var sitcom = $(this).attr("data-sitcom");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          sitcom + "&api_key=6e575d67725e411280acd5e1a3c802a6";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .done(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+          var sitcomDiv = $("<div class='item'>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var sitcomImage = $("<img>");
+          sitcomImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.append(p);
+          gifDiv.append(sitcomImage);
+
+          $("#gifs-appear-here").append(gifDiv);
+        }
+      });
+  });
