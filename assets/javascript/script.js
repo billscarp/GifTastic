29f0270eb55ef3cc3c12b6bbad921173efa8e260
billscarp <billscarp@gmail.com>
2017-09-04 18:55:08 -0400

    working on js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,69 +1,71 @@
 $(document).ready(function () {
-    // Array of Sitcoms
-    var topics = ["Friends", "That 70's Show", "Fresh Prince", "Everybody Loves Raymond", "Home Improvment", "Full House", "Simpsons", "Seinfeld", "Will & Grace", "Mash"];
-
-    // Function for displaying movie data
-    function renderButtons() {
-
-        $("#sitcoms-view").empty();
-        $.each(topics, function (index, value) {
-            var b = $("<button>")
-            b.addClass("button2")
-            b.attr("data-name", topics[index])
-            b.text(topics[index]),
-                $("#sitcoms-view").append(b);
-        });
-
-    }
-
-    // This function handles events where one button is clicked
-    $("#add-sitcom").on("click", function () {
-
-        event.preventDefault();
-
-        var newSitcom = $("#sitcom-input").val().trim();
-
-        topics.push(newSitcom);
-
-        renderButtons();
-        $("#sitcom-input").val("");
-
-    });
-
-    // Calling the renderButtons function to display the initial list of sitcoms
-
-    renderButtons();
-
+	// Array of Sitcoms
+	var topics = ["Friends", "That 70's Show", "Fresh Prince", "Everybody Loves Raymond", "Home Improvment", "Full House", "Simpsons", "Seinfeld", "Will & Grace", "Mash"];
+	
+	// Function for displaying movie data
+	function renderButtons() {
+	
+		$("#sitcoms-view").empty();
+		$.each(topics, function (index, value) {
+			var b = $("<button>")
+			b.addClass("button2")
+			b.attr("data-name", topics[index])
+			b.text(topics[index]),
+			$("#sitcoms-view").append(b);
+		});
+	
+	}
+	
+	// This function handles events where one button is clicked
+	$("#add-sitcom").on("click", function () {
+	
+		event.preventDefault();
+		
+		var newSitcom = $("#sitcom-input").val().trim();
+		
+		topics.push(newSitcom);
+		
+		renderButtons();
+		$("#sitcom-input").val("");
+		
+	});
+	
+	// Calling the renderButtons function to display the initial list of sitcoms
+	renderButtons();
+	
+	
+	
+	
+	// Here I am going to add the API and ajax the way we did in our homework to bring in the gifs
+	$(".button2").on("click", function() {
+		var sitcom = $(this).attr("data-name");
+		console.log(sitcom);
+		
+		var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=6e575d67725e411280acd5e1a3c802a6&q=" + sitcom + "&limit=10&offset=0&rating=G&lang=en";
+		
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		})
+		.done(function(response) {
+			console.log(response)
+			var results = response.data;
+			
+			for (var i = 0; i < results.length; i++) {
+				var sitcomDiv = $(".item");
+				
+				var rating = results[i].rating;
+				
+				var p = $("<p>").text("Rating: " + rating);
+				
+				var sitcomImage = $("<img>");
+				sitcomImage.attr("src", results[i].images.fixed_height.url);
+				
+				gifDiv.append(p);
+				gifDiv.append(sitcomImage);
+				
+				$("#gifs-appear-here").append("gifDiv");
+			}
+		});
+	});
 });
-
-
-    // Here I am going to add the API and ajax the way we did in our homework to bring in the gifs
-    $(".button2").on("click", function() {
-        var sitcom = $(this).attr("data-name");
-        console.log(sitcom);
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=6e575d67725e411280acd5e1a3c802a6&q=" + sitcom + "&limit=10&offset=0&rating=G&lang=en";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .done(function(response) {
-        console.log(response)
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-          var sitcomDiv = $(".item");
-
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var sitcomImage = $("<img>");
-          sitcomImage.attr("src", results[i].images.fixed_height.url);
-
-          gifDiv.append(p);
-          gifDiv.append(sitcomImage);
-
-          $("#gifs-appear-here").append(gifDiv);
-        }
-      });
-  });
